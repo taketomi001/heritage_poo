13cf80ada52eca8f915a4b7d16312bc8996a9c0e
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-02-27 10:38:30 +0100

    heritage

--- Car.java
+++ Car.java
@@ -3,6 +3,7 @@ public class Car extends Vehicule {
 public Car(String brand, int kilometers){
     super(brand, kilometers);
 }
+
     @Override
     public String doStuff() {
        return "Je suis "+brand+" et je fais vroum vroum !";
